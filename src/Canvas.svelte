f6cc117538275c5aaa1ac46125b1aa78aabcae7a
wyattlake <lakewyatt@gmail.com>
2023-11-26 22:01:20 -0800

    Canvas component

---
+++ src/Canvas.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import { onMount, onDestroy, setContext } from "svelte";
+
+    export let width: number, height: number, pixelRatio: number;
+
+    let canvas: HTMLCanvasElement;
+
+    onMount(() => {
+        let context: CanvasRenderingContext2D = canvas.getContext("2d")!;
+        context.fillStyle = "red";
+        context.fillRect(0, 0, 50, 50);
+    });
+</script>
+
+<canvas
+    bind:this={canvas}
+    width={width * pixelRatio}
+    height={height * pixelRatio}
+    style="width: {width}px; height: {height}px;"
+/>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import Canvas from "../Canvas.svelte";
+</script>
+
+<h1>Graph Theory</h1>
+<Canvas width={100} height={100} pixelRatio={1} />
