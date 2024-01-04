ceafcb343ff60187e2568c6833f598ecb464f24c
wyattlake <lakewyatt@gmail.com>
2024-01-04 03:46:14 -0800

    Reverted digitalocean changes

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "svelte-adapter-static-digitalocean";
+import adapter from "@sveltejs/adapter-auto";
 import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,16 +11,7 @@ const config = {
         // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
         // If your environment is not supported or you settled on a specific environment, switch out the adapter.
         // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-        adapter: adapter({
-            // default options are shown
-            pages: "build",
-            assets: "build",
-            fallback: "404.html",
-            precompress: false,
-            strict: true,
-            spec: ".do/spec.yaml",
-            name: "",
-        }),
+        adapter: adapter(),
     },
 };
 
